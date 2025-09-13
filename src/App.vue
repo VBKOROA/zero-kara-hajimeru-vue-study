f8158f615d40214a50dc8fe00adbb6af886b6a0a
abko <abko121115@gmail.com>
2025-09-13 19:54:56 +0900

    feat: vue reactive 실습

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,26 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { reactive } from 'vue'
 
-const count = ref<number>(0)
+interface User {
+  name: string
+  age: number
+}
 
-function increment() {
-  count.value++
+const user = reactive<User>({
+  name: 'abko',
+  age: 1,
+})
+
+function celebrateBirth() {
+  user.age++
 }
 </script>
 
 <template>
-  <h1>Vue 반응성 테스트</h1>
-  <p>현재 카운트: {{ count }}</p>
-  <button @click="increment">Up Up!</button>
+  <h1>Vue reactive 실습</h1>
+  <p>이름 : {{ user.name }}</p>
+  <p>나이 : {{ user.age }}</p>
+  <button @click="celebrateBirth">나이 추가요~</button>
 </template>
 
 <style scoped></style>
