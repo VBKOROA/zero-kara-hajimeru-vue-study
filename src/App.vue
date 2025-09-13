eb12a031f8f6c0adf24a4f35cebd51d2efae078f
abko <abko121115@gmail.com>
2025-09-13 19:50:55 +0900

    feat: vue 반응성을 사용한 카운트 구현

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const count = ref<number>(0)
+
+function increment() {
+  count.value++
+}
+</script>
+
+<template>
+  <h1>Vue 반응성 테스트</h1>
+  <p>현재 카운트: {{ count }}</p>
+  <button @click="increment">Up Up!</button>
+</template>
+
+<style scoped></style>
