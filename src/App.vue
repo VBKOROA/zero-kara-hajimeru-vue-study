be3f66d305cc336344cc4bfeef29a9d15ec050a0
abko <abko121115@gmail.com>
2025-09-13 20:14:01 +0900

    feat: 컴포넌트 분리 및 props, emit 구현

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+import JustGreeter from './component/JustGreeter.vue'
 
 const message = ref<string>('')
+
+function eventHandler(justValue: string) {
+  alert(justValue)
+}
 </script>
 
 <template>
   <h1>Vue reactive 실습</h1>
   <input v-model="message" placeholder="값 입력!" />
   <p>당신이 입력한 메시지 : {{ message }}</p>
+  <JustGreeter :name="message" @update="eventHandler"></JustGreeter>
 </template>
 
 <style scoped></style>
---
+++ src/component/JustGreeter.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+defineProps<{
+  name?: string
+}>()
+
+const emit = defineEmits<{
+  (e: 'update', justValue: string): void
+}>()
+
+function eventEmit() {
+  emit('update', '와라라라랄')
+}
+</script>
+
+<template>
+  <div class="greeting-box">
+    <p>안녕하세요! {{ name }}님! 환영합니다~~</p>
+    <button @click="eventEmit">눌러보기!</button>
+  </div>
+</template>
+
+<style scoped>
+.greeting-box {
+  width: 16rem;
+  height: 6rem;
+  background-color: burlywood;
+  border-radius: 0.4rem;
+  padding: 0 0.4rem;
+}
+</style>
